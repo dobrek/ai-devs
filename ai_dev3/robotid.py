648f1a47047a7ced4ccda5aa603f6b27daf55d24
Dobromir Kropielnicki <dobromir.kropielnicki@dka.io>
2024-11-13 19:21:50 +0100

    feat: robotid

---
+++ ai_dev3/robotid.py
@@ -0,0 +1,47 @@
+import requests
+from decouple import config
+from termcolor import colored
+from utils.api_client import send_answer
+from utils.open_ai import generate_image
+
+
+def main() -> None:
+    try:
+        robot_info = _get_robot_info()
+        image_url = _generate_robot_image(robot_info["description"])
+        send_answer(task="robotid", answer=image_url)
+    except Exception as e:
+        print("Error :(", colored(e, "red"))
+
+
+def _get_robot_info() -> dict:
+    url = f"{config('CENTRALA_URL')}/data/{config('API_KEY')}/robotid.json"
+    info = requests.get(url, timeout=10).json()
+    print("Robot info:", colored(info, "blue"))
+    return info
+
+
+def _generate_robot_image(description: str) -> str:
+    prompt = image_prompt_tpl.format(description=description)
+    return generate_image(prompt=prompt, width=1024, height=1024)
+
+
+image_prompt_tpl = """
+<prompt_objective>
+The purpose is to generate a realistic image of a fictional security robot based on a witness's description provided in the prompt.
+</prompt_objective>
+
+<prompt_rules>
+- Use the description or testimonial provided to render the image of the security robot.
+- Image must be in PNG format.
+- The style of the image should be realistic, ensuring that it doesn't stray into non-realistic interpretations.
+- Avoid including elements that are not mentioned in the provided testimony.
+</prompt_rules>
+
+<description>
+{description}
+</description>
+"""
+
+if __name__ == "__main__":
+    main()
--- ai_dev3/utils/open_ai.py
+++ ai_dev3/utils/open_ai.py
@@ -4,6 +4,7 @@ from decouple import config
 from openai import NOT_GIVEN, NotGiven, OpenAI
 from openai.types import ResponseFormatJSONObject
 from openai.types.chat import ChatCompletionMessageParam
+from termcolor import colored
 
 client = OpenAI(api_key=config("OPENAI_API_KEY"))
 
@@ -22,3 +23,17 @@ def send_chat_messages(
 ) -> str:
     completion = client.chat.completions.create(model=model, messages=messages, response_format=response_format)
     return completion.choices[0].message.content
+
+
+def generate_image(prompt: str, width: int, height: int, model: str = "dall-e-3") -> str:
+    print("Prompt:", colored(prompt, "cyan"))
+    print("Generating ...")
+    response = client.images.generate(
+        model=model,
+        prompt=prompt,
+        n=1,
+        size=f"{width}x{height}",
+        response_format="url",
+    )
+    print("Generated image:", colored(response.data, "blue"))
+    return response.data[0].url
